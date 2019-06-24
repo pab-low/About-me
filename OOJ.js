5ccb6b0c93d287fb9d69548f6f168ec4a54d5647
pablo <pabloocampopita@gmail.com>
2019-06-24 03:14:05 -0400

    examples of abstraction creating a class and example of inheritance

--- OOJ.js
+++ OOJ.js
@@ -7,29 +7,78 @@ function display (content){
 // create a function that takes a name(string) as a parameter
 // and returns "hi my name is"parameter given
 
-function name(name){
-    return "Hi, My name is " + name + ".";
+
+    
+
+
+// display(name("itachi"));
+// display(melee("Punch"));
+// display(powers("powers"));
+// display(katon("oof"));
+
+//Object oriented programming
+// - Incapsulation
+
+// OOP Incapsulation: grouping all behavior inside an object
+const ninja = {
+    name(name){
+     return "Hi, My name is " + name + ".";
+},
+
+    powers(ability){
+     return "To become a ninja you must " + ability + ".";
+
+},
+
+    katon(style){
+     return "Katon " + style + " Jutsu";
+},
+
+    melee(move){
+     return "Using my dagger " + move + "in close combat";
+ }
+}
+//display(ninja.name("itachi"));
+//display(ninja.melee("Punch"));
+//display(ninja.powers("powers"));
+//display(ninja.katon("oof"));
+
+
+//create a class for instantiating ninja's
+// OOP Abstraction: hiding complexity
+
+class Ninja { 
+    constructor(name, ability,style, move){
+     this.nameInsideNinja = name;
+     this.ability     = ability;
+     this.style        = style;
+     this.move         = move;
+   }
+
+   name(){
+    return "Hi, My name is " + this.nameInsideNinja + ".";
+    }
+  
+   powers(){
+    return "Using " + this.ability + "Sharinga";
+
+    }
+
+   Jutsu(){
+    return "Katon " + this.style + " style Jutsu";
+    }
+
+   melee(){
+    return "Using my dagger " + this.move + "in close combat";
+    } 
 }
 
-display(name("Jimmy"));
 
-// create 3 function that describe the powers of a ninja 
-// you would like to create
+// OOP inheritance: object inherit from classes
+let itachi = new Ninja("Itachi", "Magenko", "Fire", "Upside Down Knife Throw");
 
-function powers(power){
-    return "To become a ninja you must " + power + ".";
+display(itachi.Jutsu())
+display(itachi.name())
+display(itachi.melee())
+display(itachi.powers())
 
-}
-
-function katon(style){
-    return "Katon " + style + " Jutsu";
-}
-
-function melee(move){
-    return "Using my dagger " + move + "in close combat";
-}
-
-display(name("itachi"));
-display(melee("Punch"));
-display(powers("powers"));
-display(katon("oof"));
